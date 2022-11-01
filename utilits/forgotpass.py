bb6d534b76722877dd45b94b15e778597af4fdfb
EnthusiasticXcoder <115250815+EnthusiasticXcoder@users.noreply.github.com>
2022-11-01 23:41:54 +0530

    Update forgotpass.py

--- utilits/forgotpass.py
+++ utilits/forgotpass.py
@@ -28,9 +28,9 @@ def forgot(userid):
         messagebox.showerror("Error","Unable to forgot password")
 
 def sendmail(userid,password):
-    email_sender='attendence.login2022@gmail.com'
-    email_password= "xzqv ekbm roax auqi"
-    email_receiver = 'vkgupta.419@gmail.com'
+    email_sender='enter mail id'
+    email_password= "enter api password"
+    email_receiver = 'enter mail id'
 
     subject = "Request to forgot passward"
 
