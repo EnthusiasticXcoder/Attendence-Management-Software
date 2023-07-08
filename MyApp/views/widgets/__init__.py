cdf5f724beca01c59f4c99e6db3fa92e50a285c3
Anshul Verma <anshulverma1405@gmail.com>
2023-07-08 21:47:52 +0530

    creating function widgets

---
+++ MyApp/views/widgets/__init__.py
@@ -0,0 +1,6 @@
+import widgets.ToggelMenu as ToggelMenu
+import widgets.CreateSheet as CreateSheet
+import widgets.CreateNewUser as CreateNewUser
+import widgets.EnterAttendence as EnterAttendence
+import widgets.DeleteUser as DeleteUser
+import widgets.ChangePassword as ChangePassword
