b49e970b406b219bcb9f17a0155d0679438132aa
Anshul Verma <anshulverma1405@gmail.com>
2023-07-14 15:15:27 +0530

    added constants

---
+++ MyApp/utilities/constants/constants.py
@@ -0,0 +1,72 @@
+
+WORKBOOKPATH = 'WorkbookPath'
+WORKSHEET = 'Worksheet'
+
+USERNAME = 'Username'
+PASSWORD = 'Password'
+LEVELORADMIN = 'LevelorAdmin'
+ADMIN = 'Admin'
+SUBJECT = 'Request to forgot passward'
+BODY = '''
+    This mail is with the regards to the request to forget password 
+    made by a user of Attendence software .
+    the username and password for the same are given below .
+    '''
+
+ID = 'id'
+TITLE = 'title'
+
+# States
+LOGINSTATE = 'loginstate'
+ADMINSTATE = 'AdminHomeState'
+USERSTATE = 'UserHomeState'
+
+CREATENEWADMIN ='Create New Admin'
+CREATENEWUSER = 'Create New User'
+
+BOOKMEN_OLD_STYLE = 'Bookman Old Style'
+TIMES_NEW_ROMAN = 'times new roman'
+ROBOTO_MEDIUM = 'Roboto Medium'
+
+LOGIN = 'Log in'
+FORGOT_PASSWORD = 'Forgot Password ?'
+
+HOME ='Home'
+CREATE_NEW_SHEET = 'Create New Sheet'
+IMPORT_WORKBOOK = 'Import Workbook'
+ENTER_ATTENDENCE = 'Enter Attendence'
+DOWNLOAD_EXCEL = 'Download Excel'
+CREATE_NEW_ADMIN = 'Create New Admin'
+CREATE_NEW_USER = 'Create New User'
+CHANGE_PASSWORD = 'Change Password'
+DELETE_ENTRY = 'Delete Entry'
+LOGOUT = 'Log Out'
+
+NO_FILE_TO_DOWNLOAD = ' --- NO FILE TO DOWNLOAD ---'
+SELECT_WORKBOOK = 'Select Workbook'
+SHEET_NAME = 'Sheet Name'
+SAVE = 'Save'
+SELECT_CLASS = 'Select Class'
+THEORY = 'Theory'
+PRACTICLE = 'Practicle'
+TIME = 'Time'
+ENTER_ROLLNO = 'Enter RollNo :'
+CONFIRM = 'Confirm'
+SUBMIT = 'Submit'
+CANCEL = 'Cancel'
+SUCCESS = 'Sucess'
+
+ENTER_USERNAME = 'Enter UserName'
+ENTER_PASSWORD = 'Enter Password'
+CONFIRM_PASSWORD = 'Confirm Password'
+
+OLD_PASSWORD = 'Old Password'
+NEW_PASSWORD = 'New Password'
+CONFIRM_NEW_PASSWORD = 'Confirm New Password'
+
+DELETE_ACCOUNT = 'Delete Account'
+
+SESSONS = ("9:45-10:35","10:35-11:25","11:30-12:20","12:20-1:10","1:40-2:30","2:30-3:20","3:20-4:05","4:05-4:50")
+BOLD = 'bold'
+
+ATTENDENCE_ENTERED_SUCCESSFULLY = 'Attendence Entered Sucessfully'
